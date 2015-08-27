8dfecfb4dc7dcbac7cebd9c7ac0ee4d15e9d3101
hel-sys <helsinquedeveloper@gmail.com>
2015-08-26 22:58:43 -0300

    update

---
+++ app/Repositories/ProjectRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace myProject\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface ProjectRepository
+ * @package namespace myProject\Repositories;
+ */
+interface ProjectRepository extends RepositoryInterface
+{
+    //
+}
---
+++ database/migrations/2015_08_26_023211_create_project_tables.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('owner_id')->usigened();
+            $table->foreign('owner_id')->references('id')->on('users');
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->string('name');
+            $table->text('description');
+            $table->smallInteger('progress')->usingened();
+            $table->smallInteger('status')->usingened();
+            $table->date('due_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects');
+    }
+}
