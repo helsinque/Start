40a90e0c3e274ef26352c6fe085cd070bfdbf1a1
helsinque <helsinquedeveloper@gmail.com>
2015-09-09 23:41:13 -0300

    Update routes.php
    rotas que faltaram [CRUD]

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,3 +24,6 @@ Route::put('client/{id}', 'ClientController@update');
 
 Route::get('project','ProjectController@index');
 Route::get('project/{id}','ProjectController@show');
+Route::post('project', 'ProjectController@store');
+Route::put(' project/{id}', 'ProjectController@update');
+Route::delete('project/{id}', 'ProjectController@destroy');
